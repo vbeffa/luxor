556db65bac6337a93b611564169408c0cd38885d
Vlad Beffa <vbeffa@mac.com>
2021-02-05 20:11:14 -0600

    refactor, tests, subscriptions table

--- etc/postgres/0_init.sql
+++ etc/postgres/0_init.sql
@@ -1,14 +1,13 @@
-
---- change / add / modify this schema as necessary
-CREATE TABLE public.sample_table (
-    id bigint NOT NULL,
-    created_at timestamp without time zone DEFAULT now() NOT NULL,
-    sample VARCHAR(255)
-);
-
 CREATE TABLE auth_requests (
-  id int NOT NULL,
+  id bigint NOT NULL,
   username VARCHAR(255),
   pass_hash VARCHAR(255),
-  req_ts TIMESTAMP
+  created_at timestamp WITHOUT time zone DEFAULT now() NOT NULL
+);
+
+CREATE TABLE subscriptions (
+  id bigint NOT NULL,
+  subscription_id_1 VARCHAR(32),
+  subscription_id_2 VARCHAR(32),
+  extra_nonce_1 VARCHAR(32)
 );
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module vbeffa/luxor
 go 1.15
 
 require (
-	github.com/lib/pq v1.9.0 // indirect
-	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	github.com/lib/pq v1.9.0
+	github.com/onsi/ginkgo v1.15.0
+	github.com/onsi/gomega v1.10.5
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
 )
